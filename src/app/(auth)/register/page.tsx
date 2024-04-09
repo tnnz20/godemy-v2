375e727595791287489186673f98febb3f808c3c
tnnz20 <kaitora200@gmail.com>
2024-04-09 09:03:07 +0800

    feat: add register route

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import RegisterCard from "./_component/register-card"
+
+export default function RegisterPage() {
+  return (
+    <div className="container flex h-screen w-full items-center justify-center">
+      <RegisterCard></RegisterCard>
+    </div>
+  )
+}
