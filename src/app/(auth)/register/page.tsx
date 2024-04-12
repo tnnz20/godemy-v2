7261784bd9e444aa415a91743f5be79d683e4f48
tnnz20 <kaitora200@gmail.com>
2024-04-12 23:05:30 +0800

    Refactor import statement for RegisterCard component in register/page.tsx

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react"
 
-import RegisterCard from "./_component/register-card"
+import RegisterCard from "./_components/register-card"
 
 export default function RegisterPage() {
   return (
