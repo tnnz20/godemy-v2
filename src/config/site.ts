b8a08ed6d43cae299130bc5f2594da81e5848447
tnnz20 <kaitora200@gmail.com>
2024-04-15 22:22:10 +0800

    feat: set site config for metadata

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Godemy",
+  url: "https://godemy.vercel.app",
+  description:
+    "E-learning platform to learn Golang with dashboard and interactive feature.",
+  author: "tnnz20",
+  links: {
+    github: "https://github.com/tnnz20",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
