0351d5961b30b5b8668c2b03fa847b383be41f69
tnnz20 <kaitora200@gmail.com>
2024-04-16 21:46:45 +0800

    feat: Add NotFound component for handling 404 errors in chapters

---
+++ src/app/(platform)/chapters/[...slug]/not-found.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+import { Icons } from "@/components/icons"
+
+export default function NotFound() {
+  const router = useRouter()
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-2">
+      <Icons.Warning className="h-20 w-20 text-accent" />
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p>Halaman Tidak ditemukan.</p>
+      <Button variant={"default"} className={cn("mt-2")} onClick={() => router.back()}>
+        Kembali
+      </Button>
+    </main>
+  )
+}
