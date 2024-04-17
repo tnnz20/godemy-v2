5dbdf8e561efa6e952525494681fb0988c32a6ae
tnnz20 <kaitora200@gmail.com>
2024-04-17 08:19:24 +0800

    refactor: fix styling not-found ts

--- src/app/(platform)/chapters/[...slug]/not-found.tsx
+++ src/app/(platform)/chapters/[...slug]/not-found.tsx
@@ -10,13 +10,13 @@ import { Icons } from "@/components/icons"
 export default function NotFound() {
   const router = useRouter()
   return (
-    <main className="flex h-full flex-col items-center justify-center gap-2">
+    <div className="relative -top-16 flex w-full flex-col items-center justify-center gap-2">
       <Icons.Warning className="h-20 w-20 text-accent" />
       <h2 className="text-xl font-semibold">404 Not Found</h2>
       <p>Halaman Tidak ditemukan.</p>
       <Button variant={"default"} className={cn("mt-2")} onClick={() => router.back()}>
         Kembali
       </Button>
-    </main>
+    </div>
   )
 }
