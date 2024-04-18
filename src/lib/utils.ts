fbda0cdaeb02f551e763531ab6e466442157f96b
tnnz20 <kaitora200@gmail.com>
2024-04-18 12:38:37 +0800

    feat: add new utils index to alphabet

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function indexToAlphabet(index: number) {
+  const charCodeA = "A".charCodeAt(0)
+  return String.fromCharCode(charCodeA + index)
 }
