5f33077084070c9a42d9369f37551c162a839066
tnnz20 <kaitora200@gmail.com>
2024-04-18 08:23:35 +0800

    style: fix not found style

--- src/app/(platform)/chapters/[...slug]/not-found.tsx
+++ src/app/(platform)/chapters/[...slug]/not-found.tsx
@@ -10,7 +10,7 @@ import { Icons } from "@/components/icons"
 export default function NotFound() {
   const router = useRouter()
   return (
-    <div className="relative -top-16 flex w-full flex-col items-center justify-center gap-2">
+    <div className="flex h-svh w-full flex-col items-center justify-center gap-2 md:pb-32">
       <Icons.Warning className="h-20 w-20 text-accent" />
       <h2 className="text-xl font-semibold">404 Not Found</h2>
       <p>Halaman Tidak ditemukan.</p>
