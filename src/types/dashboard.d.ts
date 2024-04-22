c749803832980099882cc8445a58b3895e48e4f1
tnnz20 <kaitora200@gmail.com>
2024-04-22 21:00:08 +0800

    feat: add types for dashboard

---
+++ src/types/dashboard.d.ts
@@ -0,0 +1,9 @@
+import { Icons } from "next/dist/lib/metadata/types/metadata-types"
+
+export type Role = "student" | "teacher"
+
+export type NavLinkDashboard = {
+  name: string
+  href: string
+  icon: SVGElement | ForwardRefExoticComponent<LucideProps>
+}
