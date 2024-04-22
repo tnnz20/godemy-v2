e40c64e81d78c53aab5ceb91b6a918b861491677
tnnz20 <kaitora200@gmail.com>
2024-04-22 22:06:20 +0800

    feat: add dashboard header

---
+++ src/app/(platform)/dashboard/_components/dashboard-header.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Role } from "@/types/dashboard"
+
+import BreadcrumbHeader from "./breadcrumb-header"
+import DashboardMobileNav from "./dashboard-mobile-nav"
+
+type DashboardHeaderProps = {
+  role: Role
+}
+
+export default function DashboardHeader({ role }: Readonly<DashboardHeaderProps>) {
+  return (
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+      <DashboardMobileNav role={role} />
+      <BreadcrumbHeader />
+    </header>
+  )
+}
--- src/app/(platform)/dashboard/layout.tsx
+++ src/app/(platform)/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 
 import { Role } from "@/types/dashboard"
 
+import DashboardHeader from "./_components/dashboard-header"
 import DashboardSideNav from "./_components/dashboard-sidenav"
 
 interface DocsLayoutProps {
@@ -17,11 +18,14 @@ export const metadata: Metadata = {
 }
 
 export default function DocsLayout({ children }: Readonly<DocsLayoutProps>) {
-  const role: Role = "student"
+  const role: Role = "teacher"
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <DashboardSideNav role={role} />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">{children}</div>
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <DashboardHeader role={role} />
+        {children}
+      </div>
     </div>
   )
 }
