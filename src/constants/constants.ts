2f07e5f4b521c48379df252548b06f21486dc759
tnnz20 <kaitora200@gmail.com>
2024-05-13 23:29:39 +0800

    feat: add constant error

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,8 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:5000"
+
+export const ErrValidation = "validation error"
+export const ErrUserNotFound = "user not found"
+export const ErrInternalServer = "server error"
+export const ErrInvalid = "invalid error"
+export const ErrConflict = "conflict error"
