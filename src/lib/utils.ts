1038736c7c4b8ca3af38e6e65bcb0a5f4141c8da
tnnz20 <kaitora200@gmail.com>
2024-05-13 23:29:28 +0800

    feat: add utils to Decode JWT

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,10 @@ export function indexToAlphabet(index: number) {
   const charCodeA = "A".charCodeAt(0)
   return String.fromCharCode(charCodeA + index)
 }
+
+export function DecodeJWT(token: string = "") {
+  const decodableToken = token.split(".")[1] ?? ""
+  const decoded = Buffer.from(decodableToken, "base64").toString()
+
+  return JSON.parse(decoded)
+}
