bde7a1363e5465dd54cba4ef1cb4e49c98c7fe93
tnnz20 <kaitora200@gmail.com>
2024-05-14 13:56:46 +0800

    feat(Auth): add header in layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,8 @@
-import React from "react"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+
+import { Icons } from "@/components/icons"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -6,10 +10,22 @@ interface LayoutProps {
 
 export default function LayoutAuth({ children }: Readonly<LayoutProps>) {
   return (
-    <div className="flex flex-col">
-      {/* <NavigationHome /> */}
-      <div className="container flex h-screen w-full items-center justify-center">{children}</div>
-      {/* <Footer /> */}
+    <div className="flex min-h-screen flex-col">
+      <HeaderLayout />
+      <div className="container flex w-full flex-grow items-center justify-center">{children}</div>
     </div>
+  )
+}
+
+function HeaderLayout() {
+  return (
+    <header className="mx-4 mb-auto mt-2">
+      <Button variant={"ghost"} asChild>
+        <Link href="/" className="flex gap-1">
+          <Icons.ChevronLeft />
+          <h2>Home</h2>
+        </Link>
+      </Button>
+    </header>
   )
 }
