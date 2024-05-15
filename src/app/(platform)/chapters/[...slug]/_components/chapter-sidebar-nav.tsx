e26df9a85549db846ff00b38c8cb76ae28a5903d
tnnz20 <kaitora200@gmail.com>
2024-05-15 13:28:54 +0800

    feat(Chapter): add condition based progress user

--- src/app/(platform)/chapters/[...slug]/_components/chapter-sidebar-nav.tsx
+++ src/app/(platform)/chapters/[...slug]/_components/chapter-sidebar-nav.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { motion } from "framer-motion"
 
 import { ChaptersConfig, ChapterSubNavItem } from "@/types/chapters"
 import { cn } from "@/lib/utils"
@@ -13,21 +12,23 @@ import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 
 type ChapterSidebarNavProps = {
-  chaptersConfig: ChaptersConfig
+  chapters: ChaptersConfig
+  progress: number
 }
 
-export default function ChapterSidebarNav({ chaptersConfig }: Readonly<ChapterSidebarNavProps>) {
-  const items = chaptersConfig.NavItems
+export default function ChapterSidebarNav({ chapters, progress }: Readonly<ChapterSidebarNavProps>) {
+  const items = chapters.NavItems
   const pathname = usePathname()
   const [showSideNav, setShowSideNav] = useState<boolean>(true)
 
   const currentOpenPage = items.find((section) => section.items?.some((item) => item.href === pathname))
   const currentIndexPage = items.findIndex((section) => section?.title === currentOpenPage?.title)
+
   return (
     <>
       {!showSideNav ? (
         <Button
-          aria-label="SideNav Open Toggle"
+          aria-label="Sidebar Nav Open Toggle"
           onClick={() => setShowSideNav(true)}
           className="border-r-lg fixed -right-3 top-20 z-50 hidden md:inline-block"
         >
@@ -37,13 +38,13 @@ export default function ChapterSidebarNav({ chaptersConfig }: Readonly<ChapterSi
 
       {items.length ? (
         <div
-          className={cn("fixed -right-1/2 hidden h-screen overflow-y-auto border-l px-4 py-6 md:block", {
+          className={cn("fixed -right-1/2 hidden overflow-y-auto border-l px-4 py-6 md:block", {
             "sticky left-0 duration-300 animate-in slide-in-from-right-1/2": showSideNav,
           })}
         >
           <div className="mr-2 flex items-center justify-between">
             <Button
-              aria-label="SideNav Close Toggle"
+              aria-label="Sidebar Nav Close Toggle"
               variant="ghost"
               size="icon"
               className={cn("px-1 transition")}
@@ -59,7 +60,9 @@ export default function ChapterSidebarNav({ chaptersConfig }: Readonly<ChapterSi
                 <div key={item.title}>
                   <AccordionItem value={"item-" + index} className="m-2">
                     <AccordionTrigger className={cn("flex gap-4")}>{item.title}</AccordionTrigger>
-                    {item.items ? <ChapterSidebarNavItem items={item.items} pathname={pathname} /> : null}
+                    {item.items ? (
+                      <ChapterSidebarNavItem items={item.items} pathname={pathname} progress={progress} />
+                    ) : null}
                   </AccordionItem>
                 </div>
               ))}
@@ -74,14 +77,14 @@ export default function ChapterSidebarNav({ chaptersConfig }: Readonly<ChapterSi
 type ChapterSidebarNavItem = {
   items: ChapterSubNavItem[]
   pathname: string | null
+  progress: number
 }
 
-export function ChapterSidebarNavItem({ items, pathname }: ChapterSidebarNavItem) {
+export function ChapterSidebarNavItem({ items, pathname, progress }: ChapterSidebarNavItem) {
   return items.length ? (
     <AccordionContent className={cn("ml-3")}>
       {items.map((item) =>
-        //TODO: change disabled to threshold
-        !item.disabled && item.href ? (
+        progress >= item.threshold ? (
           <Link
             key={item.title}
             href={item.href}
@@ -92,9 +95,13 @@ export function ChapterSidebarNavItem({ items, pathname }: ChapterSidebarNavItem
             {item.title}
           </Link>
         ) : (
-          <span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60" key={item.title}>
+          <Link
+            key={item.title}
+            href={item.href}
+            className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60"
+          >
             {item.title}
-          </span>
+          </Link>
         )
       )}
     </AccordionContent>
