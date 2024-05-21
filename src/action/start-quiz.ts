9dac3bfadc1667d96b9bc78a33da6b8920e2d5fb
tnnz20 <kaitora200@gmail.com>
2024-05-21 19:41:28 +0800

    refactor: change fetch url and add throw error

--- src/action/start-quiz.ts
+++ src/action/start-quiz.ts
@@ -6,23 +6,22 @@ import { BASE_URL } from "@/constants/constants"
 
 export async function StartQuiz(token: string, array: number[], id: string) {
   try {
-    const response = await fetch(
-      `${BASE_URL}/assessments/assessment/create/users`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ assessment_code: id, random_array_id: array }),
-      }
-    )
+    const response = await fetch(`${BASE_URL}/assessments/assessment/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ assessment_code: id, random_array_id: array }),
+    })
     const data = await response.json()
     if (response.status !== 201) {
-      throw new Error(data?.error?.error_description || "Internal server error")
+      throw new Error(
+        `status: ${response.status}, message: ${data?.error?.error_description}`
+      )
     }
   } catch (error) {
-    console.error(error)
+    console.log(error)
   }
   revalidatePath("/chapters/quiz/started")
   redirect(`/chapters/quiz/started?quiz=${id}`)
