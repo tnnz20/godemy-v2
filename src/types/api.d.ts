7f726ec5d5c8e251294cd2cb744768dc04776a43
tnnz20 <kaitora200@gmail.com>
2024-05-21 19:43:27 +0800

    feat: add type for API response

---
+++ src/types/api.d.ts
@@ -0,0 +1,13 @@
+export type UserAssessmentResult = {
+  code: number
+  message: string
+  data: {
+    id: UUID
+    users: UUID
+    courses_id: UUID
+    assessment_code: string
+    assessment_value: number
+    created_at: Date
+    updated_at: Date
+  }
+}
