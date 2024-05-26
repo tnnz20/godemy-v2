f32410ec67fff394b33cc96c3e168abfd8bc7f7b
tnnz20 <kaitora200@gmail.com>
2024-05-27 02:41:13 +0800

    feat: add dashboard student table and remove props

--- src/app/(platform)/dashboard/(overview)/_components/dashboard-student-wrapper.tsx
+++ src/app/(platform)/dashboard/(overview)/_components/dashboard-student-wrapper.tsx
@@ -1,19 +1,15 @@
-import { Role } from "@/types/dashboard"
-
 import DashboardProfile from "./dashboard-profile"
 import DashboardStudentCard from "./dashboard-student-card"
+import DashboardStudentTable from "./dashboard-student-table"
 
-interface DashboardStudentWrapperProps {
-  role: Role
-}
-
-export default function DashboardStudentWrapper({ role }: Readonly<DashboardStudentWrapperProps>) {
+export default function DashboardStudentWrapper() {
   return (
     <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <DashboardStudentCard />
+        <DashboardStudentTable />
       </div>
-      <DashboardProfile role={role} />
+      <DashboardProfile />
     </div>
   )
 }
