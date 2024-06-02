5278bb4556e48b995aa9a40b7f704eeb4c3e1a8d
tnnz20 <kaitora200@gmail.com>
2024-06-02 14:24:05 +0800

    feat(Dashboard): implement API

--- src/app/(platform)/dashboard/(overview)/_components/dashboard-teacher-card.tsx
+++ src/app/(platform)/dashboard/(overview)/_components/dashboard-teacher-card.tsx
@@ -1,31 +1,49 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { cookies } from "next/headers"
+import Link from "next/link"
 
-export default function DashboardTeacherCard() {
-  const kelas = 2
-  const kelasA = 10
-  const kelasB = 5
+import { GetTotalCourses } from "@/lib/GetTotalCourses"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default async function DashboardTeacherCard() {
+  const cookiesStore = cookies()
+  const token = cookiesStore.get("token")?.value
+  const totalCourses = await GetTotalCourses(token as string)
+  const total = totalCourses?.data?.total
   return (
     <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-      <Card x-chunk="dashboard-05-chunk-1">
-        <CardHeader className="pb-2">
-          <CardDescription>Kelas</CardDescription>
-          <CardTitle className="text-4xl">{kelas}</CardTitle>
+      <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+        <CardHeader className="pb-3">
+          <CardTitle>Kelola kelas</CardTitle>
+          <CardDescription className="max-w-lg text-balance leading-relaxed">
+            {total
+              ? "Kelola kelas yang dimiliki, dan lihat progres belajar siswa."
+              : "Anda belum memiliki kelas. Silahkan buat kelas terlebih dahulu."}
+          </CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className="text-xs text-muted-foreground">Anda mengelola {kelas}</div>
-        </CardContent>
+        <CardFooter>
+          {total ? (
+            <Button asChild>
+              <Link href="/dashboard/daftar-kelas">Lihat kelas</Link>
+            </Button>
+          ) : (
+            <Button asChild>
+              <Link href="/dashboard/buat-kelas">Buat kelas</Link>
+            </Button>
+          )}
+        </CardFooter>
       </Card>
-      <Card x-chunk="dashboard-05-chunk-2">
-        <CardHeader className="pb-2">
-          <CardDescription>Jumlah siswa</CardDescription>
-          <CardTitle className="text-4xl">{kelasA + kelasB}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="text-xs text-muted-foreground">
-            Jumlah tersebut berdasarkan penjumlahan dari {kelas} kelas
-          </div>
-        </CardContent>
-      </Card>
+      {!total ? null : (
+        <Card x-chunk="dashboard-05-chunk-1">
+          <CardHeader className="pb-2">
+            <CardDescription>Kelas</CardDescription>
+            <CardTitle className="text-4xl">{total}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-xs text-muted-foreground">jumlah kelas yang dimiliki</div>
+          </CardContent>
+        </Card>
+      )}
     </div>
   )
 }
