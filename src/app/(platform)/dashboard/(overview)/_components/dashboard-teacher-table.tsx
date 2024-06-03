01eb30da4dbd3f34fc08b8a6e9387c24f4c3dbef
tnnz20 <kaitora200@gmail.com>
2024-06-03 08:31:11 +0800

    feat(Dashboard): add table teacher

---
+++ src/app/(platform)/dashboard/(overview)/_components/dashboard-teacher-table.tsx
@@ -0,0 +1,22 @@
+import { CoursesResultData } from "@/types/api"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+import TableClass from "../../daftar-kelas/_components/table"
+
+type DashboardTeacherTableProps = {
+  courses: CoursesResultData[]
+}
+
+export default function DashboardTeacherTable({ courses }: Readonly<DashboardTeacherTableProps>) {
+  return (
+    <Card x-chunk="dashboard-02-chunk-3">
+      <CardHeader>
+        <CardTitle>Daftar Kelas</CardTitle>
+        <CardDescription>Daftar kelas yang dimiliki.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <TableClass courses={courses} />
+      </CardContent>
+    </Card>
+  )
+}
