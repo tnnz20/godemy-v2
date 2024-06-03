0575188491a67bcc022ccdea33263825dbdccf22
tnnz20 <kaitora200@gmail.com>
2024-06-03 08:31:00 +0800

    feat(Dashboard): update layout for teacher and add teacher table

--- src/app/(platform)/dashboard/(overview)/_components/dashboard-teacher-wrapper.tsx
+++ src/app/(platform)/dashboard/(overview)/_components/dashboard-teacher-wrapper.tsx
@@ -1,13 +1,23 @@
+import { cookies } from "next/headers"
+
+import { GetCourses } from "@/lib/GetCourses"
+
 import DashboardProfile from "./dashboard-profile"
 import DashboardTeacherCard from "./dashboard-teacher-card"
+import DashboardTeacherTable from "./dashboard-teacher-table"
 
-export default function DashboardTeacherWrapper() {
+export default async function DashboardTeacherWrapper() {
+  const cookieStore = cookies()
+  const token = cookieStore.get("token")?.value
+
+  const courses = await GetCourses(token as string)
   return (
     <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <DashboardTeacherCard />
-        <DashboardProfile />
+        <DashboardTeacherTable courses={courses?.data} />
       </div>
+      <DashboardProfile />
     </div>
   )
 }
