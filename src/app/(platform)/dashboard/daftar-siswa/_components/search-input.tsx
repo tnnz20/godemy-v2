d5b7e49c0357fb8d3922bed8e93b0606649287d5
tnnz20 <kaitora200@gmail.com>
2024-06-03 12:01:47 +0800

    feat(Dashboard) init search input component

---
+++ src/app/(platform)/dashboard/daftar-siswa/_components/search-input.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
+
+import { Input } from "@/components/ui/input"
+
+import { Icons } from "@/components/icons"
+
+export default function SearchStudent() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams)
+    params.set("page", "1")
+    if (term) {
+      params.set("name", term)
+    } else {
+      params.delete("name")
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
+  return (
+    <div className="relative ml-auto flex-1 md:grow-0">
+      <Icons.Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+      <Input
+        type="search"
+        placeholder="Search..."
+        defaultValue={searchParams.get("name")?.toString()}
+        onChange={(e) => handleSearch(e.target.value)}
+        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+      />
+    </div>
+  )
+}
