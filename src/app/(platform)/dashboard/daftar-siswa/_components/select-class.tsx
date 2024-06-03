f707c268b9451d8dad11fa9483aa0f1dd9322cbb
tnnz20 <kaitora200@gmail.com>
2024-06-03 12:02:04 +0800

    feat(Dashboard): change logic to store value into params

--- src/app/(platform)/dashboard/daftar-siswa/_components/select-class.tsx
+++ src/app/(platform)/dashboard/daftar-siswa/_components/select-class.tsx
@@ -1,21 +1,33 @@
 "use client"
 
-import { useShallow } from "zustand/react/shallow"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 import { CoursesResultData } from "@/types/api"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-
-import { useClassStore } from "../_store/useClassStore"
 
 type SelectClassProps = {
   courses: CoursesResultData[]
 }
 
 export default function SelectClass({ courses }: Readonly<SelectClassProps>) {
-  const [setCourseId] = useClassStore(useShallow((state) => [state.setCourseId]))
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const courseId = searchParams.get("courseId")?.toString()
+  const handleChange = (courseId: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (courseId) {
+      params.set("courseId", courseId)
+    } else {
+      params.delete("courseId")
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
 
   return (
-    <Select onValueChange={setCourseId}>
+    <Select onValueChange={handleChange} defaultValue={courseId}>
       <SelectTrigger className="w-[240px]" aria-label="Select courses">
         <SelectValue placeholder="Pilih Kelas" />
       </SelectTrigger>
