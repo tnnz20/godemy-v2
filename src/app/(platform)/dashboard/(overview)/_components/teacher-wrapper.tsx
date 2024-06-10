3ee28f4b8794978334856fda0988b326456f9ee1
tnnz20 <kaitora200@gmail.com>
2024-06-10 15:18:09 +0800

    refactor(Dashboard): move GetCourses lib

--- src/app/(platform)/dashboard/(overview)/_components/teacher-wrapper.tsx
+++ src/app/(platform)/dashboard/(overview)/_components/teacher-wrapper.tsx
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers"
 
-import { GetCourses } from "@/lib/GetCourses"
+import { GetCourses } from "@/lib/data/courses/course"
 
 import DashboardProfile from "./profile-card"
 import DashboardTeacherCard from "./teacher-card"
@@ -11,6 +11,7 @@ export default async function DashboardTeacherWrapper() {
   const token = cookieStore.get("token")?.value
 
   const courses = await GetCourses(token as string)
+
   return (
     <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
--- src/lib/data/courses/course.ts
+++ src/lib/data/courses/course.ts
@@ -23,3 +23,27 @@ export async function GetTotalCourses(token: string) {
     console.log(error)
   }
 }
+
+export async function GetCourses(token: string) {
+  try {
+    const response = await fetch(`${BASE_URL}/courses`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else if (response.status === 404) {
+      return data?.code
+    } else {
+      throw new Error(
+        `status: ${response.status}, message: ${data?.error?.error_description}`
+      )
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
