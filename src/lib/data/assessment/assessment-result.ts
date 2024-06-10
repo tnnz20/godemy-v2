cfb73703cdb91b9cc7b7df9a71ff4a18bee05c42
tnnz20 <kaitora200@gmail.com>
2024-06-10 13:43:58 +0800

    refactor: move GetUserAssessmentsResults lib

--- src/lib/data/assessment/assessment-result.ts
+++ src/lib/data/assessment/assessment-result.ts
@@ -31,3 +31,29 @@ export async function GetUserAssessmentResult(
     console.log(error)
   }
 }
+
+export async function GetUserAssessmentsResults(token: string) {
+  try {
+    const response = await fetch(`${BASE_URL}/assessments`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      cache: "no-cache",
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      return data
+    } else if (response.status === 404) {
+      return data?.code
+    } else {
+      throw new Error(
+        `status: ${response.status}, message: ${data?.error?.error_description}`
+      )
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
