a0ef5c911faececa81ee382c439578d74b8b332f
tnnz20 <kaitora200@gmail.com>
2024-06-11 12:30:53 +0800

    refactor: GetCourses Params

--- src/app/(platform)/dashboard/(overview)/_components/teacher-wrapper.tsx
+++ src/app/(platform)/dashboard/(overview)/_components/teacher-wrapper.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers"
 
+import { CourseResultData } from "@/types/api"
 import { GetCourses } from "@/lib/data/courses/course"
 
 import DashboardProfile from "./profile-card"
@@ -10,13 +11,13 @@ export default async function DashboardTeacherWrapper() {
   const cookieStore = cookies()
   const token = cookieStore.get("token")?.value
 
-  const courses = await GetCourses(token as string)
+  const courses = await GetCourses(token as string, 1)
 
   return (
     <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <DashboardTeacherCard />
-        <DashboardTeacherTable courses={courses?.data} />
+        <DashboardTeacherTable courses={courses?.data as CourseResultData[]} />
       </div>
       <DashboardProfile />
     </div>
