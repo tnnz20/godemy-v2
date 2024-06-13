5f6ebed95c626a93587c9f94de1160be5aba4f13
tnnz20 <kaitora200@gmail.com>
2024-06-13 17:59:27 +0800

    refactor(Auth): add image for login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Image from "next/image"
 
 import LoginCard from "./_components/card"
 
@@ -8,5 +9,18 @@ export const metadata: Metadata = {
 }
 
 export default function LoginPage() {
-  return <LoginCard />
+  return (
+    <div className="w-full lg:grid lg:min-h-[100px] lg:grid-cols-2 xl:min-h-[200px]">
+      <LoginCard />
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/static/growth.webp"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-auto object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
 }
