64f55858af6610764b227bf35244c9872bd02807
tnnz20 <kaitora200@gmail.com>
2024-06-14 01:31:08 +0800

    refactor: make start-quiz-button to server components

--- src/app/(platform)/chapters/[...slug]/_components/mdx-components/start-quiz-button.tsx
+++ src/app/(platform)/chapters/[...slug]/_components/mdx-components/start-quiz-button.tsx
@@ -1,85 +1,44 @@
-"use client"
-
-import { use } from "react"
-import { useParams } from "next/navigation"
-import { StartQuiz } from "@/action/quiz"
-
-import { UserAssessmentResultData } from "@/types/api"
+import { UserAssessmentResult, UserAssessmentResultData } from "@/types/api"
 import { GetUserAssessmentResult } from "@/lib/data/assessment/assessment-result"
 import { CheckAssessmentValue, FormattedDate } from "@/lib/utils"
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
+import QuizButton from "./quiz-button"
 
 interface ChapterStartQuizButtonProps {
   token: string | undefined
+  paramsId: string
 }
 
-export function ChapterStartQuizButton({ token }: Readonly<ChapterStartQuizButtonProps>) {
-  const params = useParams()
-  const paramId = params?.slug[0]
-
-  const maxLength = paramId === "7" ? 20 : 5
+export async function ChapterStartQuizButton({ token, paramsId }: Readonly<ChapterStartQuizButtonProps>) {
+  const maxLength = paramsId === "7" ? 20 : 5
   const randomArrayId = generateRandomArray(1, maxLength)
 
-  const userAssessmentResult = use(GetUserAssessmentResult(parseInt(paramId), token as string))
-
-  const isPassedQuiz = CheckAssessmentValue(userAssessmentResult)
-
-  const handleStartQuiz = async () => {
-    await StartQuiz(token as string, randomArrayId, paramId)
-  }
+  const usersAssessmentResults = await GetUserAssessmentResult(token as string, parseInt(paramsId))
+  const isQuizPassed = CheckAssessmentValue(usersAssessmentResults as UserAssessmentResult)
 
   return (
     <div className="my-8 flex flex-col items-center justify-center gap-8">
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button disabled={isPassedQuiz}>Mulai kuis</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Apakah anda yakin akan memulai kuis?</AlertDialogTitle>
-            <AlertDialogDescription>
-              Ketika kuis dimulai anda tidak dapat kembali ke halaman sebelumnya.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Batal</AlertDialogCancel>
-            <AlertDialogAction>
-              <button onClick={async () => await handleStartQuiz()}>Lanjutkan</button>
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-      {userAssessmentResult === 404 ? (
+      <QuizButton
+        token={token as string}
+        paramsId={paramsId}
+        randomArrayId={randomArrayId}
+        isQuizPassed={isQuizPassed}
+      />
+      {usersAssessmentResults?.code === 404 ? (
         <TableCaption>Tidak ada riwayat kuis</TableCaption>
       ) : (
-        <HistoryUserAssessment userAssessmentResult={userAssessmentResult?.data} />
+        <HistoryUserAssessment userAssessmentResult={usersAssessmentResults?.data as UserAssessmentResultData[]} />
       )}
     </div>
   )
 }
 interface HistoryUserAssessmentProps {
-  userAssessmentResult: UserAssessmentResultData
+  userAssessmentResult: UserAssessmentResultData[]
 }
 
 function HistoryUserAssessment({ userAssessmentResult }: Readonly<HistoryUserAssessmentProps>) {
-  const assessmentValue = userAssessmentResult?.assessment_value
-  const status = assessmentValue >= 80 ? "Lulus" : "Tidak Lulus"
-  const date = String(userAssessmentResult?.created_at)
-  const formattedDate = FormattedDate(date)
-
   return (
     <Table>
       <TableHeader>
@@ -91,16 +50,27 @@ function HistoryUserAssessment({ userAssessmentResult }: Readonly<HistoryUserAss
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow key={"user history"}>
-          <TableCell className="font-medium">{formattedDate}</TableCell>
-          <TableCell>{userAssessmentResult?.assessment_code}</TableCell>
-          <TableCell>{assessmentValue}</TableCell>
-          <TableCell>
-            <Badge className="text-xs" variant={status === "Lulus" ? "success" : "destructive"}>
-              {status}
-            </Badge>
-          </TableCell>
-        </TableRow>
+        {userAssessmentResult
+          ? userAssessmentResult.map((userAssessmentResult) => {
+              const assessmentValue = userAssessmentResult?.assessment_value
+              const date = String(userAssessmentResult?.created_at)
+              const formattedDate = FormattedDate(date)
+
+              const status = userAssessmentResult?.status
+              return (
+                <TableRow key={userAssessmentResult?.id}>
+                  <TableCell className="font-medium">{formattedDate}</TableCell>
+                  <TableCell>{userAssessmentResult?.assessment_code}</TableCell>
+                  <TableCell>{assessmentValue}</TableCell>
+                  <TableCell>
+                    <Badge className="text-xs" variant={status === 1 ? "success" : "destructive"}>
+                      {status === 1 ? "Lulus" : "Tidak Lulus"}
+                    </Badge>
+                  </TableCell>
+                </TableRow>
+              )
+            })
+          : null}
       </TableBody>
     </Table>
   )
