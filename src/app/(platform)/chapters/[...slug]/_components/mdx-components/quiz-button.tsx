571c02f3ff855a4d2a8c2a5797c5feaad7c50e84
tnnz20 <kaitora200@gmail.com>
2024-06-14 01:31:25 +0800

    feat(Quiz): add quiz button component

---
+++ src/app/(platform)/chapters/[...slug]/_components/mdx-components/quiz-button.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { StartQuiz } from "@/action/quiz"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+
+interface QuizButtonProps {
+  token: string
+  paramsId: string
+  randomArrayId: number[]
+  isQuizPassed: boolean
+}
+
+export default function QuizButton({ token, paramsId, randomArrayId, isQuizPassed }: QuizButtonProps) {
+  const handleStartQuiz = async () => {
+    await StartQuiz(token as string, randomArrayId, paramsId)
+  }
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button disabled={isQuizPassed}>Mulai kuis</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Apakah anda yakin akan memulai kuis?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Ketika kuis dimulai anda tidak dapat kembali ke halaman sebelumnya.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Batal</AlertDialogCancel>
+          <AlertDialogAction>
+            <button onClick={async () => await handleStartQuiz()}>Lanjutkan</button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
