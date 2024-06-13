8c4cc00b7cda39e5793c999f44c83fa3bd3dbf2f
tnnz20 <kaitora200@gmail.com>
2024-06-13 18:00:01 +0800

    refactor(Login): update login card layout and content

--- src/app/(auth)/login/_components/card.tsx
+++ src/app/(auth)/login/_components/card.tsx
@@ -1,31 +1,25 @@
 import Link from "next/link"
 
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-
 import LoginForm from "./form"
 
 export default function LoginCard() {
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>Masukan email dan password yang sudah terdaftar untuk masuk ke akun anda</CardDescription>
-      </CardHeader>
-      <CardContent>
+    <div className="flex items-center justify-center">
+      <div className="mx-auto grid w-[350px] gap-6">
+        <div className="grid gap-2 text-center">
+          <h1 className="text-3xl font-bold">Media Pembelajaran Pemrograman Golang</h1>
+          <p className="text-balance text-muted-foreground">
+            Masukan email dan password yang sudah terdaftar untuk masuk ke akun anda
+          </p>
+        </div>
         <LoginForm />
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        <CardDescription>
+        <div className="mt-4 text-center text-sm">
           Belum punya akun?{" "}
-          <span className="hover:text-foreground hover:underline hover:underline-offset-4">
-            <Button variant="link" className="p-0 text-accent-foreground" asChild>
-              <Link href={"/register"}>Daftar sekarang</Link>
-            </Button>
-            <span className="sr-only">Button Register Page</span>
-          </span>
-        </CardDescription>
-      </CardFooter>
-    </Card>
+          <Link href="/register" className="underline">
+            Daftar Sekarang
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
