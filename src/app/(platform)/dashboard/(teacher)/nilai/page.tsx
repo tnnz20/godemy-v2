49ecb9f9f65fa18cc4eddd2dca8d641a8fd712c6
tnnz20 <kaitora200@gmail.com>
2024-06-15 00:05:04 +0800

    init nilai page

---
+++ src/app/(platform)/dashboard/(teacher)/nilai/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Nilai",
+  description: "Score page detail student score in godemy platform",
+}
+
+export default function ScorePage() {
+  return <div>NilaiPage</div>
+}
