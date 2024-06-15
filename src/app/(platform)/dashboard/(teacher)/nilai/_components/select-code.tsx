55cf9eb267730ee1d62550109b99f99d94988bb0
tnnz20 <kaitora200@gmail.com>
2024-06-15 22:27:51 +0800

    feat(Dashboard): add component select-code

---
+++ src/app/(platform)/dashboard/(teacher)/nilai/_components/select-code.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export default function SelectCode() {
+  const codeMap = {
+    1: "Chapter 1",
+    2: "Chapter 2",
+    3: "Chapter 3",
+    4: "Chapter 4",
+    5: "Chapter 5",
+    6: "Chapter 6",
+    7: "Evaluasi",
+  }
+
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleChange = (value: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (value) {
+      params.set("code", value)
+    } else {
+      params.delete("courseId")
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
+
+  const code = searchParams.get("code")?.toString() ?? ""
+
+  return (
+    <Select onValueChange={handleChange} defaultValue={code}>
+      <SelectTrigger className="w-[240px]" aria-label="Select courses">
+        <SelectValue placeholder="Pilih Kategori" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Pilih Kategori</SelectLabel>
+          {Object.entries(codeMap).map(([key, value]) => (
+            <SelectItem key={key} value={key}>
+              {value}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
