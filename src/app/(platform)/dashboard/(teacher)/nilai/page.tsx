422a5b6996eb4288a238812c7682f93846db98f3
tnnz20 <kaitora200@gmail.com>
2024-06-15 22:29:35 +0800

    refactor(Dashboard): implement component ScorePage

--- src/app/(platform)/dashboard/(teacher)/nilai/page.tsx
+++ src/app/(platform)/dashboard/(teacher)/nilai/page.tsx
@@ -1,4 +1,14 @@
 import { Metadata } from "next"
+import { cookies } from "next/headers"
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+import SelectClass from "../siswa/_components/select-class"
+import Filter from "./_components/filter-dropdown"
+import ScorePagination from "./_components/pagination"
+import SearchStudent from "./_components/search-input"
+import SelectCode from "./_components/select-code"
+import ScoreTable from "./_components/table"
 
 export const metadata: Metadata = {
   title: "Nilai",
@@ -6,5 +16,34 @@ export const metadata: Metadata = {
 }
 
 export default function ScorePage() {
-  return <div>NilaiPage</div>
+  const cookiesStore = cookies()
+  const token = cookiesStore.get("token")?.value
+
+  return (
+    <div className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-8">
+      <div className="mt-4 flex items-center">
+        <SelectClass token={token as string} />
+        <div className="ml-auto flex flex-col items-center gap-2">
+          <div className="ml-auto flex items-center gap-2">
+            <SelectCode />
+            <Filter />
+          </div>
+          <SearchStudent />
+        </div>
+      </div>
+      <Card x-chunk="dashboard-01-chunk-0">
+        <CardHeader>
+          <CardTitle>Daftar nilai siswa</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-4">
+            <ScoreTable token={token as string} />
+          </div>
+        </CardContent>
+        <CardFooter className="flex items-end justify-end">
+          <ScorePagination token={token as string} />
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
