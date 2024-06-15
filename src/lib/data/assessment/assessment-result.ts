0c810a4ace1ef22927bf7b611ff9bef3404fadc6
tnnz20 <kaitora200@gmail.com>
2024-06-15 22:26:34 +0800

    feat: add new lib GetAssessmentResultUsers and GetTotalAssessmentResultUsers

--- src/lib/data/assessment/assessment-result.ts
+++ src/lib/data/assessment/assessment-result.ts
@@ -1,6 +1,10 @@
 import { BASE_URL } from "@/constants/constants"
 
-import { UserAssessmentResult } from "@/types/api"
+import {
+  AssessmentResultUsers,
+  TotalDataResponse,
+  UserAssessmentResult,
+} from "@/types/api"
 
 export async function GetUserAssessmentResult(
   token: string,
@@ -56,5 +60,75 @@ export async function GetUserAssessmentsResults(token: string) {
     }
   } catch (error) {
     console.log(error)
+  }
+}
+
+export async function GetAssessmentResultUsers(
+  token: string,
+  courseId: string,
+  assessment_code: string,
+  status: string,
+  name: string,
+  sort: string,
+  page: string
+): Promise<AssessmentResultUsers> {
+  const nextItem = parseInt(page) === 1 ? 0 : (parseInt(page) - 1) * 6
+  try {
+    const response = await fetch(
+      `${BASE_URL}/assessments/assessment/${courseId}?assessment_code=chap-${assessment_code}&name=${name}&status=${status}&sort=${sort}&offset=${nextItem}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        next: { tags: ["assessment-result"] },
+      }
+    )
+
+    const data = await response.json()
+    return data
+  } catch (error: any) {
+    console.log(error)
+    return {
+      code: error.code,
+      error: {
+        error_name: "InternalServerError",
+        error_description: error.message,
+      },
+    }
+  }
+}
+
+export async function GetTotalAssessmentResultUsers(
+  token: string,
+  courseId: string,
+  assessment_code: string,
+  name: string
+): Promise<TotalDataResponse> {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/assessments/assessment/${courseId}/total?assessment_code=chap-${assessment_code}&name=${name}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        next: { tags: ["assessment-result"] },
+      }
+    )
+
+    const data = await response.json()
+    return data
+  } catch (error: any) {
+    console.log(error)
+    return {
+      code: error.code,
+      error: {
+        error_name: "InternalServerError",
+        error_description: error.message,
+      },
+    }
   }
 }
