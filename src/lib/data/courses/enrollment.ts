1be4e2ec6e7ed4aebe1871a285fb74c59ba3ab73
tnnz20 <kaitora200@gmail.com>
2024-07-01 19:45:11 +0800

    refactor: move enrolled users details and add return types

--- src/lib/data/courses/enrollment.ts
+++ src/lib/data/courses/enrollment.ts
@@ -1,6 +1,6 @@
 import { BASE_URL } from "@/constants/constants"
 
-import { TotalDataResponse } from "@/types/api"
+import { EnrolledUsersDetails, TotalDataResponse } from "@/types/api"
 
 export async function GetCourseEnrollmentDetail(token: string | undefined) {
   try {
@@ -48,6 +48,37 @@ export async function GetTotalEnrolledUsers(
     return data as TotalDataResponse
   } catch (error: any) {
     console.log(error)
+    return {
+      code: 500,
+      error: {
+        error_name: "InternalServerError",
+        error_description: error.message,
+      },
+    }
+  }
+}
+
+export async function GetEnrolledUsersDetails(
+  courseId: string,
+  name: string,
+  page: number
+): Promise<EnrolledUsersDetails> {
+  try {
+    const nextItem = page === 1 ? 0 : (page - 1) * 6
+    const response = await fetch(
+      `${BASE_URL}/courses/course/${courseId}/enrolled?name=${name}&offset=${nextItem}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-cache",
+      }
+    )
+
+    const data = await response.json()
+    return data as EnrolledUsersDetails
+  } catch (error: any) {
     return {
       code: 500,
       error: {
